aa7025b14f1ab3bfbc86d584789dfdcba97f26c5
Joe simpson <joe@egmrc.com>
2024-08-28 09:23:09 -0600

    Update home page layout

--- app/page.tsx
+++ app/page.tsx
@@ -62,25 +62,31 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center">
             <Header/>
             <div className="bg-metal bg-cover w-full">
-                <div className="flex flex-wrap md:flex-nowrap items-start justify-center overflow-x-hidden">
+                <div className="flex flex-wrap md:flex-nowrap items-start justify-center">
                     <Image src={necklace} alt="" className="-rotate-12 shrink md:w-96 w-64 -translate-y-20 sm:-mr-24 order-1 md:order-none"/>
-                    <div className="bg-white p-16 px-24 my-auto w-full max-w-screen-md z-10 md:z-auto mt-14 md:mt-0">
+                    <div className="bg-white p-16 px-24 w-full max-w-screen-md z-10 md:z-auto mt-auto mb-6">
                         <Image className="w-full mb-4" src={logo} alt="Wheeler Manufacturing company Incorporated Est. 1946"/>
                         <p className="font-light text-lg uppercase text-center text-gray-500">Manufacturing Company Incorporated, EST. 1946</p>
                     </div>
-                    <div className="flex-col mt-auto sm:-ml-14 mb-24 hidden md:flex order-2">
+                    <div className="flex-col mt-auto sm:-ml-14 -mb-20 hidden md:flex order-2 shrink-0">
                         <Image src={ring1} alt="" className="-scale-y-100 -rotate-180 w-44 -mb-12"/>
                         <Image src={ring2} alt="" className="w-44 sm:-translate-x-1/2"/>
                     </div>
                 </div>
 
                 <div className="flex">
-                    <h2 className="bg-gray-500 bg-opacity-80 text-white pl-8 pt-2 md:pt-6 pr-16 pb-4 text-lg md:text-4xl leading-snug">
-                        {"North America's"} jewelry wholesale leader<br className="hidden md:block"/> in the tourist, souvenir and travel industry.
-                    </h2>
+                    <div
+                        className="flex z-20 bg-gray-500 min-w-[50%] font-light bg-opacity-80 text-white pl-8 pt-2 md:pt-10 pr-8 pb-2 md:pb-10 text-3xl md:text-5xl leading-snug">
+                        <h2 className="ml-auto">
+                            {"North America's"} jewelry wholesale leader<br className="hidden md:block"/> in the
+                            tourist, souvenir and travel industry.
+                        </h2>
+                    </div>
+
                 </div>
                 <div className="bg-gray-950 p-4 my-24">
-                    <div className="flex max-w-screen-2xl gap-1 gap-y-3 flex-wrap mx-auto justify-around md:justify-evenly">
+                    <div
+                        className="flex max-w-screen-2xl gap-1 gap-y-3 flex-wrap mx-auto justify-around md:justify-evenly">
                         <Link href="/wheeler-jewelry"><Image src={wheeler} alt=""/></Link>
                         <Link href="/rings"><Image src={ringsByWheeler} alt=""/></Link>
                         <Link href="/wheeler-jewelry"><Image src={copperByWheeler} alt=""/></Link>
